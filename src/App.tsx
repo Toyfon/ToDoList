3ed9d0fd967bd25e3db51a5eb112c06d1442d79d
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-12-02 11:09:57 +0300

    add  add tasks-reducer.ts

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export type TodolistType = {
     filter: FilterValuesType
 }
 
-type TaskStateType = {
+export type TaskStateType = {
     [key: string]: taskType[]
 }
 
--- src/Store/todo-reducer.ts
+++ src/Store/todo-reducer.ts
@@ -34,18 +34,27 @@ export const toDoReducer = (state: TodolistType[], action: ActionType): Todolist
 }
 
 
-export type ActionType = ReturnType<typeof removeTodoListAC> |
-                         ReturnType<typeof addTodoListAC> |
-                         ReturnType<typeof changeTodoListTitleAC> |
-                         ReturnType<typeof changeTodoListFilterAC>
+export type ActionType = RemoveTodoListACType |
+                         AddTodoListACType |
+                         ChangeTodoListTitleACType |
+                         ChangeTodoListFilterACType
 
+export type RemoveTodoListACType = ReturnType<typeof removeTodoListAC>
 export const removeTodoListAC = (id: string) => ({type: "REMOVE-TODOLIST", id} as const)
+
+export type AddTodoListACType = ReturnType<typeof addTodoListAC>
 export const addTodoListAC = (newTodolistTitle: string, id:string) => ({type: "ADD-TODOLIST", title: newTodolistTitle, id} as const)
+
+
+export type ChangeTodoListTitleACType = ReturnType<typeof changeTodoListTitleAC>
 export const changeTodoListTitleAC = (id: string, title: string) => ({
     type: "CHANGE-TODOLIST_TITLE",
     title,
     id
 } as const)
+
+
+export type ChangeTodoListFilterACType = ReturnType<typeof changeTodoListFilterAC>
 export const changeTodoListFilterAC = (id: string, filter: FilterValuesType) => ({
     type: "CHANGE-TODOLIST_FILTER",
     filter,
